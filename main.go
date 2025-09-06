470ab82b24685b487bc048df5e98bc8e3af2d9af
side <viewfromaside@gmail.com>
2025-09-06 15:56:09 -0300

    [core] connection to server made

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/nundb/nundb"
+)
+
 func main() {
+	nundb_client := nundb.NewClient("ws://localhost:3012/", "user", "pwd")
+
+	fmt.Println(nundb_client)
 }
--- nundb/service.go
+++ nundb/service.go
@@ -1,1 +1,34 @@
 package nundb
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	Name       string
+	connection *websocket.Conn
+}
+
+func NewClient(serverUrl, name, password string) *Client {
+	u, err := url.Parse(serverUrl)
+	if err != nil {
+		log.Fatal("error parsing url: ", err)
+	}
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("error during connection: ", err)
+	}
+	defer c.Close()
+
+	fmt.Println("connected to: ", u.String())
+
+	return &Client{
+		Name:       name,
+		connection: c,
+	}
+}
