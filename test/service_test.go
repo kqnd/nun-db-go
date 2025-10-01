b531271656a560c2acbb3929852fbcd22f698234
kqnd <viewfromaside@gmail.com>
2025-09-30 23:18:39 -0300

    [core] getting value test

--- test/service_test.go
+++ test/service_test.go
@@ -12,3 +12,20 @@ func TestNewClient(t *testing.T) {
 		t.Fatalf("client could not be nil, err: %s", err)
 	}
 }
+
+func TestGettingValues(t *testing.T) {
+	client, err := nundb.NewClient("ws://localhost:3012", "user-name", "user-pwd")
+	if err != nil {
+		t.Fatalf("client could not be nil, err: %s", err)
+	}
+
+	client.Set("foo", "bar")
+	foo, err := client.Get("foo")
+	if err != nil {
+		t.Fatalf("foo could not be nil, err: %s", err)
+	}
+
+	if foo != "bar" {
+		t.Fatalf("foo value is different that bar")
+	}
+}
